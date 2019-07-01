a49e76a04406c6f785208697fcc357081aff9668
Rupinder Kaur <rupinderk.sidhu30@gmail.com>
2019-07-01 18:26:13 -0400

    GPIO Buttons

---
+++ GPIO_buttons/source/GPIO_buttons.c
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2016-2018 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+/**
+ * @file    GPIO_buttons.c
+ * @brief   Application entry point.
+ */
+
+#include "board.h"
+#include "fsl_debug_console.h"
+#include "fsl_gpio.h"
+
+#include "pin_mux.h"
+#include <stdbool.h>
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define APP_BOARD_TEST_LED_PORT 1U
+#define APP_BOARD_TEST_LED_PIN 10U
+
+#define SWITCH1_SPEED_LED_PORT 1U
+#define SWITCH1_SPEED_LED_PIN 3U
+
+#define SWITCH1_DIRECTION_LED_PORT 1U
+#define SWITCH1_DIRECTION_LED_PIN 1U
+
+#define SWITCH2_SPEED_LED_PORT 1U
+#define SWITCH2_SPEED_LED_PIN 7U
+
+#define SWITCH12_DIRECTION_LED_PORT 1U
+#define SWITCH12_DIRECTION_LED_PIN 9U
+
+#define APP_SW_PORT BOARD_SW1_GPIO_PORT
+#define APP_SW_PIN  BOARD_SW1_GPIO_PIN
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*!
+ * @brief delay a while.
+ */
+void delay(void);
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+void delay(void)
+{
+    volatile uint32_t i = 0;
+    for (i = 0; i < 100000; ++i)
+    {
+        __asm("NOP"); /* delay */
+    }
+}
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    uint32_t port_state = 0;
+
+    /* Define the init structure for the output LED pin*/
+    gpio_pin_config_t led_config = {
+        kGPIO_DigitalOutput, 0,
+    };
+
+    /* Board pin, clock, debug console init */
+    /* attach 12 MHz clock to FLEXCOMM0 (debug console) */
+    CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
+    /* enable clock for GPIO*/
+    CLOCK_EnableClock(kCLOCK_Gpio0);
+    CLOCK_EnableClock(kCLOCK_Gpio1);
+
+    BOARD_InitPins();
+    BOARD_BootClockFROHF48M();
+    BOARD_InitDebugConsole();
+
+    /* Print a note to terminal. */
+    PRINTF("\r\n GPIO Driver example\r\n");
+    PRINTF("\r\n The LED is taking turns to shine.\r\n");
+
+    /* Init SW GPIO PORT. */
+    GPIO_PortInit(GPIO, APP_SW_PORT);
+
+    /* Init output LED GPIO. */
+    GPIO_PortInit(GPIO, APP_BOARD_TEST_LED_PORT);
+    GPIO_PinInit(GPIO, APP_BOARD_TEST_LED_PORT, APP_BOARD_TEST_LED_PIN, &led_config);
+    GPIO_PinWrite(GPIO, APP_BOARD_TEST_LED_PORT, APP_BOARD_TEST_LED_PIN, 1);
+
+    GPIO_PortInit(GPIO, APP_BOARD_TEST_LED_PORT);
+    GPIO_PinInit(GPIO, APP_BOARD_TEST_LED_PORT, APP_BOARD_TEST_LED_PIN, &led_config);
+    GPIO_PinWrite(GPIO, APP_BOARD_TEST_LED_PORT, APP_BOARD_TEST_LED_PIN, 1);
+
+    GPIO_PortInit(GPIO, APP_BOARD_TEST_LED_PORT);
+    GPIO_PinInit(GPIO, APP_BOARD_TEST_LED_PORT, APP_BOARD_TEST_LED_PIN, &led_config);
+    GPIO_PinWrite(GPIO, APP_BOARD_TEST_LED_PORT, APP_BOARD_TEST_LED_PIN, 1);
+
+    GPIO_PortInit(GPIO, APP_BOARD_TEST_LED_PORT);
+    GPIO_PinInit(GPIO, APP_BOARD_TEST_LED_PORT, APP_BOARD_TEST_LED_PIN, &led_config);
+    GPIO_PinWrite(GPIO, APP_BOARD_TEST_LED_PORT, APP_BOARD_TEST_LED_PIN, 1);
+
+    /* Port masking */
+    GPIO_PortMaskedSet(GPIO, APP_BOARD_TEST_LED_PORT, 0x0000FFFF);
+    GPIO_PortMaskedWrite(GPIO, APP_BOARD_TEST_LED_PORT, 0xFFFFFFFF);
+    port_state = GPIO_PortRead(GPIO, APP_SW_PORT);
+    PRINTF("\r\n Standard port read: %x\r\n", port_state);
+    port_state = GPIO_PortMaskedRead(GPIO, APP_SW_PORT);
+    PRINTF("\r\n Masked port read: %x\r\n", port_state);
+
+    while (1)
+    {
+        port_state = GPIO_PortRead(GPIO, APP_SW_PORT);
+        if (!(port_state & (1 << APP_SW_PIN)))
+        {
+            PRINTF("\r\n Port state: %x\r\n", port_state);
+            GPIO_PortToggle(GPIO, APP_BOARD_TEST_LED_PORT, 1u << APP_BOARD_TEST_LED_PIN);
+        }
+        delay();
+    }
+}
